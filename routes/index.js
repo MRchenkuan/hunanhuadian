ae9845d4ffb6eca24971ffe69b19da861118bb64
chenkuan <393667111@qq.com>
2017-06-27 16:49:12 +0800

    初始化

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '湖南华电首页' });
+});
+
+module.exports = router;
