965ac92a7cfd3b27e860f0a77d7ea35cf9136afd
chenkuan <393667111@qq.com>
2017-08-03 14:40:51 +0800

    公司概况

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,16 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: '湖南华电首页' });
 });
-
+router.get('/distribution', function(req, res, next) {
+    res.render('introductions/distribution', { title: '业务及分布' });
+});
+router.get('/about', function(req, res, next) {
+    res.render('introductions/about', { title: '公司简介' });
+});
+router.get('/speech', function(req, res, next) {
+    res.render('introductions/speech', { title: '总经理致辞' });
+});
+router.get('/leadership', function(req, res, next) {
+    res.render('introductions/leadership', { title: '总经理致辞' });
+});
 module.exports = router;
