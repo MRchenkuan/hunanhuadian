06b28e29f855893dc82dae897cd581d88a822fc5
chenkuan <393667111@qq.com>
2017-08-04 16:10:47 +0800

    公司概况

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,32 @@ router.get('/speech', function(req, res, next) {
 router.get('/leadership', function(req, res, next) {
     res.render('introductions/leadership', { title: '总经理致辞' });
 });
+router.get('/culture', function(req, res, next) {
+    res.render('cultures/constructure', { title: '文化体系' });
+});
+router.get('/rules', function(req, res, next) {
+    res.render('cultures/rules', { title: '员工守则' });
+});
+
+router.get('/photos', function(req, res, next) {
+    res.render('cultures/photos', { title: '电厂美景' });
+});
+
+router.get('/hire', function(req, res, next) {
+    res.render('hr/hire', { title: '人才招聘' });
+});
+
+router.get('/training', function(req, res, next) {
+    res.render('hr/training', { title: '教育培训' });
+});
+
+router.get('/trends', function(req, res, next) {
+    res.render('news/trends', { title: '行业动态' });
+});
+router.get('/basenews', function(req, res, next) {
+    res.render('news/basenews', { title: '基层新闻' });
+});
+router.get('/importance', function(req, res, next) {
+    res.render('news/importance', { title: '公司要闻' });
+});
 module.exports = router;
