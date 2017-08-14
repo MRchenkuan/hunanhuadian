e067b4f25a46946bbb7a557f0059a118dbda2c1e
chenkuan <393667111@qq.com>
2017-08-14 23:19:49 +0800

    相册处理

--- public/javascripts/modal.js
+++ public/javascripts/modal.js
@@ -2,7 +2,7 @@
  * Created by chenkuan on 2017/8/4.
  */
 (function (w) {
-    w.showNews = function (id) {
+    w.showNews = function (me) {
         var content = document.createElement("pre");
         var title = document.createElement("p");
         var banner = document.createElement("img");
@@ -19,10 +19,9 @@
         title.style.width ="100%"
         banner.style.width ="100%"
 
-        title.innerText = "疾风迅雷  再展雄姿";
-        // banner.src = "/public/images/img25.png";
-        banner.src = id;
-        content.innerText = "\t宜章是全国有名的红炮之乡，湖南华电福新太平里风力发电有限公司在宜章这片热土上实现了开门红，捷报频传，喜事不断，犹如鲜花绽放、春色满园。太平里一期风电场的建设成功和良好的效益，没有停止创业者的前进步伐。他们又有了新的设想，那就是要一鼓作气，乘势而上，早日拿下太平里二期风电场的建设\n\t集结号、冲锋号再次在宜章县白石渡镇吹响。二期工程安装25台南车集团生产的WT116型机组，等效满负荷小时数2025h。2016年10月首台基础浇筑，2016年12月首台风机并网发电。\n\t成功和喜悦写在脸上，安全和质量始终牢记心中。湖南华电福新太平里风力发电有限公司二期风电项目各施工单位总体质量控制体系健全，施工人员严格落实安全生产岗位岗位责任制，严格把好质量各个关口，各类质量安全目标得到了有效的贯彻和落实。工程质量过关和安全平稳，各项控制指标均得到监控并运行在正常范围内。太平里风电二期工程从浇筑第一个风电机基础开始，到2017年2月已经完成15个风机基础。“山下旌旗在望,山头鼓角相闻”二期项目又将于2017年8月全面完工并投产。"
+        title.innerText = me.getAttribute("data-title")
+        banner.src = me.getAttribute("data-banner");
+        content.innerText = me.getAttribute("data-content")
 
         showModal({
             title:title,
--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res, next) {
           {title:'湖南华电郴州风力发电有限公司工会开展“赴一线、送清凉”活动',banner:'/public/images/home/news5@2x.jpg'}
       ],
       newsCards:[
-          {banner:"/public/images/img1@2x.jpg",title:"总经理致辞",text:"湖南华电郴州风力发电有限公司作为湖南首家风力发电企业,带动了湖南绿色清洁能源产业，拥有难得的机遇和发展空间，但同时也面对很多挑战……",url:"speech"},
+          {banner:"/public/images/img1.jpg",title:"总经理致辞",text:"湖南华电郴州风力发电有限公司作为湖南首家风力发电企业,带动了湖南绿色清洁能源产业，拥有难得的机遇和发展空间，但同时也面对很多挑战……",url:"speech"},
           {banner:"/public/images/img2@2x.jpg",title:"公司简介",text:"湖南华电郴州风力发电有限公司于2009年6月11日注册成立，是湖南省首家建设投产的风力发电公司……",url:"about"},
           {banner:"/public/images/img3@2x.jpg",title:"领导班子",text:"公司领导班子共4人，设有公司总经理兼党支部书记1人、副总经理3人……",url:"leadership"},
           {banner:"/public/images/img4@2x.jpg",title:"业务及分布",text:"公司仰天湖36.3MW风电项目于2008年8月通过湖南省发展和改革委员会核准批复，2010年1月3日投产……",url:"distribution"}
